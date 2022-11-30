fa8d3edb72c9de9b330ffb8994e231e6f29d4f30
Mathurin BODY <mathurin.body@gmail.com>
2022-11-30 12:10:44 +0100

    Ajout lint

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet } from "react-native";
-import { Provider as PaperProvider } from "react-native-paper";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { TabBar } from "./src/Tab";
+import {NavigationContainer} from '@react-navigation/native';
+import {StatusBar} from 'expo-status-bar';
+import * as React from 'react';
+import {Provider as PaperProvider} from 'react-native-paper';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 export default function App() {
   return (
@@ -11,18 +10,8 @@ export default function App() {
       <SafeAreaProvider>
         <PaperProvider>
           <StatusBar style="auto" />
-          <TabBar />
         </PaperProvider>
       </SafeAreaProvider>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 10,
-    backgroundColor: "#fff",
-    alignItems: "stretch",
-  },
-});
